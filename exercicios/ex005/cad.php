aa698dfa22d8afbfdf6e4c2033d7f77e268f7570
luizdevfelipe <lipefavaro2005@gmail.com>
2023-10-30 16:08:32 -0300

    formulário básico

---
+++ exercicios/ex005/cad.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado do processamento</h1>
+    </header>
+
+    <main>
+        <?php 
+            $nome = $_GET["nome"] ?? "sem nome";
+            $snome = $_GET["sobrenome"] ?? "desconhecido";
+            $idade = $_GET["idade"] ?? "sem idade";
+
+            echo "É um grande prazer te conhecer $nome $snome !";
+
+        ?>
+        <p>
+            <a href="javascript:history.go(-1)">Voltar para a página anterior</a>
+        </p>
+    </main>
+</body>
+</html>
