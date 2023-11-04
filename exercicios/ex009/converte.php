93d95864c96eeda893181560e94c0a4a9e9ea735
luizdevfelipe <lipefavaro2005@gmail.com>
2023-11-04 15:19:40 -0300

    cotação utilizando api

---
+++ exercicios/ex009/converte.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Conversor de Moedas</h1>
+        <?php
+        $reais = $_POST["dinheiro"] ?? 0;
+
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $fim = date("m-d-Y");
+
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $fim . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+        $dados = json_decode(file_get_contents($url), true);
+
+        $cotacao =  $dados["value"][0]["cotacaoCompra"];
+
+        $dolares = $reais / $cotacao;
+
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+        echo "Seus " . numfmt_format_currency($padrao, $reais, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolares, "USD") . "</strong>"
+
+
+
+        ?>
+        <button onclick="window.location.href='index.html'">Voltar</button>
+    </main>
+</body>
+
+</html>
