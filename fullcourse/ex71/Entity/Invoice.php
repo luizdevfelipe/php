235fa3e3e28502f5c6c15077cde524f95b03df4f
luizdevfelipe <lipefavaro2005@gmail.com>
2024-03-23 21:40:49 -0300

    Doctrine ORM
    Utilizando LifecyclesEvents para automatizar a inserção da data atual da transação

---
+++ fullcourse/ex71/Entity/Invoice.php
@@ -0,0 +1,98 @@
+<?php 
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Enums\InvoiceStatus;
+use DateTimeZone;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+
+#[Entity()]
+#[Table('invoices')]
+#[HasLifecycleCallbacks]
+class Invoice
+{
+    #[Id]
+    #[Column(), GeneratedValue()]
+    private int $id;
+
+    #[Column(type: Types::DECIMAL, precision: 10, scale: 4)]
+    private float $amount;
+
+    #[Column(name: 'user_id')]
+    private int $userId;
+
+    #[Column(name: 'created_at')]
+    private \DateTime $createdAt;
+
+    #[Column()]
+    private InvoiceStatus $status;
+
+    #[OneToMany(targetEntity: InvoiceItem::class, mappedBy: 'invoice', cascade: ['persist', 'remove'])]
+    private Collection $items;
+
+    public function __construct()
+    {   
+        $this->items = new ArrayCollection();
+    }
+
+    #[PrePersist]
+    public function onPrePersist(LifecycleEventArgs $args)
+    {
+        $this->createdAt = new \DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getAmount(): float {
+        return $this->amount;
+    }
+
+    public function getUserId(): int {
+        return $this->userId;
+    }
+
+    public function getStatus(): InvoiceStatus {
+        return $this->status;
+    }
+
+    public function setAmount(float $amount): Invoice {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function setUserId(int $userId): Invoice {
+        $this->userId = $userId;
+        return $this;
+    }
+
+    public function setStatus(InvoiceStatus $status): Invoice {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function addItem(InvoiceItem $item): Invoice
+    {
+        $item->setInvoice($this);
+        $this->items->add($item);
+        return $this;
+    }
+
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+}
---
+++ fullcourse/ex71/Entity/InvoiceItem.php
@@ -0,0 +1,61 @@
+<?php 
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Enums\InvoiceStatus;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity()]
+#[Table('invoice_items')]
+class InvoiceItem
+{
+    #[Id]
+    #[Column(), GeneratedValue()]
+    private int $id;
+
+    #[Column(name: 'invoice_id')]
+    private int $invoiceId;
+    
+    #[Column()]
+    private string $description;
+    
+    #[Column()]
+    private int $quantity;
+
+    #[Column(name: 'unity_price', type: Types::DECIMAL, precision: 10, scale: 2)]
+    private float $unityPrice;
+
+    #[ManyToOne(inversedBy: 'items')]
+    private Invoice $invoice;
+
+    public function setDescription($desc): InvoiceItem
+    {
+        $this->description = $desc;
+        return $this;
+    }
+
+    public function setQuantity($quantity): InvoiceItem
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
+    public function setUnitPrice($price): InvoiceItem
+    {
+        $this->unityPrice = $price;
+        return $this;
+    }
+
+    public function setInvoice($invoice): InvoiceItem
+    {
+        $this->invoice = $invoice;
+        return $this;
+    }
+}
