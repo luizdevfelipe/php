a4f1c4ea30b56b5fbd6637c8a1c0bee1cc77c101
luizdevfelipe <lipefavaro2005@gmail.com>
2024-05-11 22:37:06 -0300

    Fazendo o carregamento das páginas
    Adicionando o código que faltava para funcionar a injeção de dependências através do PHP-DI suportado pelo framework Slim

--- fullcourse/ex084/public/index.php
+++ fullcourse/ex084/public/index.php
@@ -2,21 +2,39 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Config;
 use \App\Controllers\HomeController;
 use \App\Controllers\InvoiceController;
+use DI\Container;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Twig\Extra\Intl\IntlExtension;
 
+use function DI\create;
+
 define('STORAGE_PATH', __DIR__ . '/../storage');
 define('VIEWS_PATH', __DIR__ . '/../views');
+
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$container = new Container();
+
+$container->set(Config::class, create(Config::class)->constructor($_ENV));
+$container->set(EntityManager::class, fn (Config $config) => new EntityManager(
+    $config->db,
+    ORMSetup::createAttributeMetadataConfiguration([__DIR__ . '/../app/Entity'])
+));
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
 $app->get('/', HomeController::class, 'index');
+$app->get('/invoices', InvoiceController::class, 'index');
 
 $twig = Twig::create(VIEWS_PATH, [
     'cache' => STORAGE_PATH . '/cache',
