3c0dd8c1a5c0eae25b90a83fd6888384d0d6a74a
luizdevfelipe <lipefavaro2005@gmail.com>
2024-05-14 16:57:55 -0300

    Projeto rodando com Slim Framework

---
+++ fullcourse/ex85/src/eloquent.php
@@ -0,0 +1,28 @@
+<?php
+
+// declare(strict_types=1);
+
+// require_once __DIR__ . '/vendor/autoload.php';
+
+
+
+// $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
+
+// $capsule = new Capsule;
+
+// $params = [
+//     'driver' => $_ENV['DB_DRIVER'] ?? 'mysql',
+//     'host' => $_ENV['DB_HOST'],
+//     'username' => $_ENV['DB_USER'],
+//     'password' => $_ENV['DB_PASS'],
+//     'database' => $_ENV['DB_DATABASE'],
+//     'charset' => 'utf8',
+//     'collation' => 'utf8_unicode_ci',
+//     'prefix' => '',
+// ];
+
+// $capsule->addConnection($params);
+// $capsule->setEventDispatcher(new Dispatcher(new Container));
+// $capsule->setAsGlobal();
+// $capsule->bootEloquent();
---
+++ fullcourse/ex85/src/public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../configs/path_constants.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$container = require CONFIG_PATH . '/container.php';
+$router = require CONFIG_PATH . '/routes.php';
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$router($app);
+
+$app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+
+$app->run();
