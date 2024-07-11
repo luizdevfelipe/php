1afdb3ddb577398bcfbf0c01673a6336e06257f9
luizdevfelipe <lipefavaro2005@gmail.com>
2024-07-10 21:55:40 -0300

    Rename e trait
    Arquivos relativos a categorias foram renomeados no singular, além da criação de uma trait que implementa os métodos de LifeCycles utilizados pelas entidades

--- fullcourse/ex087 expennies project/resources/js/categories.js
+++ fullcourse/ex087 expennies project/resources/js/categories.js
@@ -1,27 +1,26 @@
-import { Modal } from "bootstrap"
+import { Modal }          from "bootstrap"
 import { get, post, del } from "./ajax"
-import DataTable from "datatables.net"
+import DataTable          from "datatables.net"
 
 window.addEventListener('DOMContentLoaded', function () {
     const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
-    const newCategoryModal = new Modal(document.getElementById('newCategoryModal'))
 
     const table = new DataTable('#categoriesTable', {
         serverSide: true,
         ajax: '/categories/load',
         orderMulti: false,
         columns: [
-            { data: "name" },
-            { data: "createdAt" },
-            { data: "updatedAt" },
+            {data: "name"},
+            {data: "createdAt"},
+            {data: "updatedAt"},
             {
                 sortable: false,
                 data: row => `
                     <div class="d-flex flex-">
-                        <button type="submit" class="btn btn-outline-primary delete-category-btn" data-id="${row.id}">
+                        <button type="submit" class="btn btn-outline-primary delete-category-btn" data-id="${ row.id }">
                             <i class="bi bi-trash3-fill"></i>
                         </button>
-                        <button class="ms-2 btn btn-outline-primary edit-category-btn" data-id="${row.id}">
+                        <button class="ms-2 btn btn-outline-primary edit-category-btn" data-id="${ row.id }">
                             <i class="bi bi-pencil-fill"></i>
                         </button>
                     </div>
@@ -30,21 +29,21 @@ window.addEventListener('DOMContentLoaded', function () {
         ]
     });
 
-    document.querySelector('#categoriesTable').addEventListener('click', function (event) {        
-        const editBtn = event.target.closest('.edit-category-btn')
+    document.querySelector('#categoriesTable').addEventListener('click', function (event) {
+        const editBtn   = event.target.closest('.edit-category-btn')
         const deleteBtn = event.target.closest('.delete-category-btn')
 
         if (editBtn) {
             const categoryId = editBtn.getAttribute('data-id')
 
-            get(`/categories/${categoryId}`)
+            get(`/categories/${ categoryId }`)
                 .then(response => response.json())
                 .then(response => openEditCategoryModal(editCategoryModal, response))
         } else {
             const categoryId = deleteBtn.getAttribute('data-id')
 
             if (confirm('Are you sure you want to delete this category?')) {
-                del(`/categories/${categoryId}`).then(response => {
+                del(`/categories/${ categoryId }`).then(response => {
                     if (response.ok) {
                         table.draw()
                     }
@@ -53,22 +52,10 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     })
 
-    document.querySelector('.create-category-btn').addEventListener('click', function (event) {
-        
-        post(`/categories`, {
-            name: newCategoryModal._element.querySelector('input[name="name"]').value
-        }, newCategoryModal._element).then(response => {
-            if (response.ok) {
-                table.draw()
-                newCategoryModal.hide()
-            }
-        })
-    })
-
     document.querySelector('.save-category-btn').addEventListener('click', function (event) {
         const categoryId = event.currentTarget.getAttribute('data-id')
 
-        post(`/categories/${categoryId}`, {
+        post(`/categories/${ categoryId }`, {
             name: editCategoryModal._element.querySelector('input[name="name"]').value
         }, editCategoryModal._element).then(response => {
             if (response.ok) {
@@ -79,7 +66,7 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 })
 
-function openEditCategoryModal(modal, { id, name }) {
+function openEditCategoryModal(modal, {id, name}) {
     const nameInput = modal._element.querySelector('input[name="name"]')
 
     nameInput.value = name
